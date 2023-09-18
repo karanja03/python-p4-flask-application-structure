46b13c1a02c95511a13a6e2e94fe1843dba8b05a
karanja03 <wambuik03@gmail.com>
2023-09-18 08:16:47 +0300

    Complete Lab

--- server/app.py
+++ server/app.py
@@ -3,3 +3,11 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/<string:username>')
+def index(username):
+    return f'<h1>Profile for{username}</h1>'
+
+
+if __name__ == '__main__':
+    app.run(port=5555)
